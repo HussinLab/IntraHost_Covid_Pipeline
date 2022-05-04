c2f7cc2df15ee7851f32dd9f81148c12ea1ae642
RaphaelRaphael <raphael.poujol@gmail.com>
2022-05-04 13:26:47 -0400

    updates

--- Treat_single_library.sh
+++ Treat_single_library.sh
@@ -18,7 +18,9 @@ amplicons_path=/lustre06/project/6005588/shared/References/covid-19/artic_ncov20
 ref=/project/6005588/shared/References/covid-19/NCBI/NC_045512.2/sequence.fasta
 gff=/project/6005588/shared/References/covid-19/NCBI/NC_045512.2/GCF_009858895.2_ASM985889v3_genomic.gff
 
-pe_or_se=$(fastq-dump -X 1 -Z --split-spot $file 2> /dev/null | wc -l | awk '$1!=8{print f,"single-end"}$1==8{print f,"paired-end"}' 2> /dev/null)
+pe_or_se=$(fastq-dump -X 1 -Z --split-spot $file 2> /dev/null | wc -l | awk '$1!=8{print "single-end"}$1==8{print "paired-end"}' 2> /dev/null)
+
+echo LIBRARY : $f is $pe_or_se
 
 if [ $pe_or_se == "paired-end" ]; then 
 fastq-dump -I --gzip --split-e $file
@@ -39,7 +41,7 @@ rm $f.bwa.bam $f.bwa.sam
 ${ivar_path}/ivar trim -i $f.bwa.sort.bam -b $amplicons_path -q 15 -m 30 -s 4 -p $f.bwa.amplicon_trim_smooth -e > $f.ivar.out 
 
 #Primary only
-samtools view -F 2048 -bo $f.bwa.amplicon_trim_smooth.primaryOnly.bam $f.bwa.amplicon_trim_smooth
+samtools view -F 2048 -bo $f.bwa.amplicon_trim_smooth.primaryOnly.bam $f.bwa.amplicon_trim_smooth.bam
 #sort
 samtools sort $f.bwa.amplicon_trim_smooth.primaryOnly.bam > $f.bwa.amplicon_trim_smooth.primaryOnly.sorted.bam
 rm $f.bwa.amplicon_trim_smooth.primaryOnly.bam
@@ -59,9 +61,9 @@ module use $MUGQIC_INSTALL_HOME/modulefiles
 
 module load mugqic/qualimap/2.2.1
 
-qualimap bamqc -nt 1 -outformat PDF -bam $f.bwa.sort.bam -outdir initialBam_$f --java-mem-size=10G
+qualimap bamqc -nt 1 -outformat PDF -bam $f.bwa.sort.bam -outdir initialBam_$f --java-mem-size=2G
 
-qualimap bamqc -nt 1 -outformat PDF -bam $f.bwa.amplicon_trim_smooth.primaryOnly.sorted.bam -outdir filteredBam_$f --java-mem-size=10G
+qualimap bamqc -nt 1 -outformat PDF -bam $f.bwa.amplicon_trim_smooth.primaryOnly.sorted.bam -outdir filteredBam_$f --java-mem-size=2G
 
 samtools flagstat $f.bwa.amplicon_trim_smooth.primaryOnly.sorted.bam > $f.samtools_flagstat.out
 
@@ -97,7 +99,7 @@ cat filteredBam_$f/genome_results.txt | grep "number of mapped reads" | awk '{pr
 cat filteredBam_$f/genome_results.txt | grep "first in pair" | awk '{print "mapped_reads_R1",$10}'
 cat filteredBam_$f/genome_results.txt | grep "second in pair" | awk '{print "mapped_reads_R2",$10}'
 
-cat filteredBam_$f/genome_results.txt | grep "number of duplicated reads" | awk '{print "duplicated_reads",$7}'
+cat filteredBam_$f/genome_results.txt | grep "number of duplicated reads" | awk 'BEGIN{n=0}{n=$7}END{print "duplicated_reads",n}'
 cat $f.samtools_flagstat.out | grep "properly paired"  | awk '{print "properly_paired",$1}'
 cat filteredBam_$f/genome_results.txt | grep "mean insert size" | awk '{print "mean_insert_size",$5}'
 cat filteredBam_$f/genome_results.txt | grep "median insert size" | awk '{print "median_insert_size",$5}'
@@ -105,21 +107,20 @@ cat filteredBam_$f/genome_results.txt | grep "number of A's" | awk '{print "numb
 cat filteredBam_$f/genome_results.txt | grep "number of C's" | awk '{print "number_of_C",$5}'
 cat filteredBam_$f/genome_results.txt | grep "number of G's" | awk '{print "number_of_G",$5}'
 cat filteredBam_$f/genome_results.txt | grep "number of T's" | awk '{print "number_of_T",$5}'
-cat filteredBam_$f/genome_results.txt | grep "mapped reads with deletion percentage" | awk 'BEGIN{n=0}{n=$7}END{{print "percent_mapped_reads_with_DEL",n}'
-cat filteredBam_$f/genome_results.txt | grep "mapped reads with insertion percentage" | awk 'BEGIN{n=0}{n=$7}END{{print "percent_mapped_reads_with_INS",n}'
+cat filteredBam_$f/genome_results.txt | grep "mapped reads with deletion percentage" | awk 'BEGIN{n=0}{n=$7}END{print "percent_mapped_reads_with_DEL",n}'
+cat filteredBam_$f/genome_results.txt | grep "mapped reads with insertion percentage" | awk 'BEGIN{n=0}{n=$7}END{print "percent_mapped_reads_with_INS",n}'
 cat filteredBam_$f/genome_results.txt | grep "mean coverageData" | awk '{print "mean_coverage",substr($4,1,length($4)-1)}'
-cat filteredBam_$f/genome_results.txt | grep "reference with a coverageData >= 10X" | awk '{print "percent_genome_covered_over_10X",$4}'
+cat filteredBam_$f/genome_results.txt | grep "reference with a coverageData >= 10X" | awk '{print "percent_genome_covered_over_10X",$4}' | tr ',' '.'
 
 ) | sed 's/[,%]//g'  | tr ' ' '\t' > $f.qc.tsv
 
 cp $f.bwa.amplicon_trim_smooth.primaryOnly.sorted.bam ..
 cp $f.bwa.amplicon_trim_smooth.primaryOnly.sorted.bam.bai .. 
 cp $f.qc.tsv ..
-cp $f.bwa.amplicon_trim_smooth.primaryOnly.consensus ..
+cp $f.bwa.amplicon_trim_smooth.primaryOnly.consensus.fa ..
 rm *fastq.gz
-rm SRR13268199.trimmedQ20/*fq.gz
+rm $f.trimmedQ20/*fq.gz
 cd ..
 tar -czvf $f.tar.gz $f
 rm -r $f
 fi
-echo $f is $pe_or_se
