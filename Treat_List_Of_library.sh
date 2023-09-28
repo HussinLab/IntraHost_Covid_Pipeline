05d7811a42b1398f2155cadccbb0c57bfe5aae60
RaphaelRaphael <raphael.poujol@gmail.com>
2023-09-28 14:53:17 -0400

    include mpileup and update sra-toolkit/3.0.0

--- Treat_List_Of_library.sh
+++ Treat_List_Of_library.sh
@@ -3,5 +3,5 @@
 if [ $CC_CLUSTER == "graham" ]; then 
 	parallel --jobs 32 /project/6005588/shared/bin/covid19-dev/IntraHost_Covid_Pipeline/Treat_single_library.sh {1} :::: $1 
 elif [ $CC_CLUSTER == "narval" ]; then 
-	parallel --jobs 32 /lustre06/project/6065672/shared/covid-19/RNAseq/viral/NCBI/IntraHost_Covid_Pipeline/Treat_single_library.sh {1} :::: $1 
+	parallel --jobs 40 /lustre06/project/6065672/shared/covid-19/RNAseq/viral/NCBI/IntraHost_Covid_Pipeline/Treat_single_library.sh {1} :::: $1 
 fi
--- Treat_single_library.sh
+++ Treat_single_library.sh
@@ -10,7 +10,7 @@ if [ $CC_CLUSTER == "graham" ]; then
 	module load  StdEnv/2020 gcc/9.3.0 htslib
 	pathtosratoolkit="/project/6005588/shared/bin/sra-tools/sratoolkit.2.10.8-centos_linux64/bin/"
 elif [ $CC_CLUSTER == "narval" ]; then 
-	module load  StdEnv/2020 gcc/9.3.0 sra-toolkit/2.10.8 htslib
+	module load  StdEnv/2020 gcc/9.3.0 sra-toolkit/3.0.0 htslib
 	pathtosratoolkit=""
 fi
 
@@ -66,6 +66,10 @@ samtools index $f.bwa.amplicon_trim_smooth.primaryOnly.sorted.bam
 
 samtools mpileup -d 10000 -A -Q 0 $f.bwa.amplicon_trim_smooth.primaryOnly.sorted.bam | ${ivar_path}/ivar consensus -p $f.bwa.amplicon_trim_smooth.primaryOnly.consensus -q 20 -t 0.75 -m 20 > $f.samtools.mpileup.out
 
+samtools mpileup $f.bwa.amplicon_trim_smooth.primaryOnly.sorted.bam -Q 20 -q 0 -B -A -d 600000 --fasta-ref $ref | awk '$4>100' > $f.mpileup
+perl /home/poujolra/projects/def-hussinju/shared/bin/pileup2base/pileup2base.pl -i $f.mpileup -prefix ${f}_temp
+
+mv ${f}_temp1.txt $f.base
 
 ##########################
 ############# QC PART
